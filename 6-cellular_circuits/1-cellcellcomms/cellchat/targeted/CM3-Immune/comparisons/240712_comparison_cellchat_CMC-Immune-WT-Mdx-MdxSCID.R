cad3d14823391d466b575af08718a37f8b35e348
cartal <carlos.talavera-lopez@uni-wuerzburg.de>
2024-07-12 15:37:35 +0200

    Added comparative cellchat workflow - ct5

--- 6-cellular_circuits/1-cellcellcomms/cellchat/targeted/CM3-Immune/comparisons/240712_comparison_cellchat_CMC-Immune-WT-Mdx-MdxSCID.R
+++ 6-cellular_circuits/1-cellcellcomms/cellchat/targeted/CM3-Immune/comparisons/240712_comparison_cellchat_CMC-Immune-WT-Mdx-MdxSCID.R
@@ -15,55 +15,50 @@ library(ComplexHeatmap)
 options(stringsAsFactors = FALSE)
 #trace(netClustering, edit = TRUE)
 
-use_python("/Users/cartalop/mambaforge/envs/scanpy/bin", required = TRUE)
+use_python("/Users/cartalop/miniforge3/envs/scanpy/bin/", required = TRUE)
 
 ### Read in data
 
-cellchat.H_ctrl <- readRDS("../../../data/Epithelial_Healthy-CTRL_anotated.rds")
-cellchat.H_ctrl <- updateCellChat(cellchat.H_ctrl)
-cellchat.H_ctrl
+cellchat.WT <- readRDS("/Volumes/XF-11/wurzburg/hofmann_2023/DMD-WT_CMC-Immune_cellchat_ctl240711.rds")
+cellchat.WT <- updateCellChat(cellchat.WT)
+cellchat.WT
 
-cellchat.H_iav <- readRDS("../../../data/Epithelial_Healthy-IAV_anotated.rds")
-cellchat.H_iav <- updateCellChat(cellchat.H_iav)
-cellchat.H_iav
+cellchat.Mdx <- readRDS("/Volumes/XF-11/wurzburg/hofmann_2023/DMD-Mdx_CMC-Immune_cellchat_ctl240711.rds")
+cellchat.Mdx <- updateCellChat(cellchat.Mdx)
+cellchat.Mdx
 
-cellchat.C_ctrl <- readRDS("../../../data/Epithelial_COPD-CTRL_anotated.rds")
-cellchat.C_ctrl <- updateCellChat(cellchat.C_ctrl)
-cellchat.C_ctrl
+cellchat.MdxSCID <- readRDS("/Volumes/XF-11/wurzburg/hofmann_2023/DMD-MdxSCID_CMC-Immune_cellchat_ctl240711.rds")
+cellchat.MdxSCID <- updateCellChat(cellchat.MdxSCID)
+cellchat.MdxSCID
 
-cellchat.C_iav <- readRDS("../../../data/Epithelial_COPD-IAV_anotated.rds")
-cellchat.C_iav <- updateCellChat(cellchat.C_iav)
-cellchat.C_iav
 
 ### Lift objects to level cell numbers 
 
-group.new = levels(cellchat.H_iav@idents)
-cellchat.H_ctrl <- liftCellChat(cellchat.H_ctrl, group.new)
+group.new = levels(cellchat.WT@idents)
+cellchat.Mdx <- liftCellChat(cellchat.Mdx, group.new)
 
-group.new = levels(cellchat.C_iav@idents)
-cellchat.C_ctrl <- liftCellChat(cellchat.C_ctrl, group.new)
+group.new = levels(cellchat.WT@idents)
+cellchat.MdxSCID <- liftCellChat(cellchat.MdxSCID, group.new)
 
 ### Merge objects
 
-object.list <- list(H_CTRL = cellchat.H_ctrl, H_IAV = cellchat.H_iav, C_CTRL = cellchat.C_ctrl, C_IAV = cellchat.C_iav)
+object.list <- list(WT = cellchat.WT, Mdx = cellchat.Mdx, MdxSCID = cellchat.MdxSCID)
 cellchat <- mergeCellChat(object.list, add.names = names(object.list), cell.prefix = TRUE)
 cellchat
 
 df.net <- subsetCommunication(cellchat)
+df.net
 
-unique_to_Hctrl <- setdiff(unique(df.net$H_CTRL$pathway_name), unique(df.net$H_IAV$pathway_name))
-unique_to_Hctrl
+unique_to_WT <- setdiff(unique(df.net$WT$pathway_name), unique(df.net$Mdx$pathway_name))
+unique_to_WT
 
-unique_to_Hiav <- setdiff(unique(df.net$H_IAV$pathway_name), unique(df.net$H_CTRL$pathway_name))
-unique_to_Hiav
+unique_to_Mdx <- setdiff(unique(df.net$Mdx$pathway_name), unique(df.net$WT$pathway_name))
+unique_to_Mdx
 
-unique_to_Cctrl <- setdiff(unique(df.net$C_CTRL$pathway_name), unique(df.net$C_IAV$pathway_name))
-unique_to_Cctrl
+unique_to_MdxSCID <- setdiff(unique(df.net$WT$pathway_name), unique(df.net$MdxSCID$pathway_name))
+unique_to_MdxSCID
 
-unique_to_Ciav <- setdiff(unique(df.net$C_IAV$pathway_name), unique(df.net$C_CTRL$pathway_name))
-unique_to_Ciav
-
-all_unique <- c(unique_to_Hctrl, unique_to_Hiav, unique_to_Cctrl, unique_to_Ciav)
+all_unique <- c(unique_to_WT, unique_to_Mdx, unique_to_MdxSCID)
 all_unique
 
 ### Differential number of interactions or interaction strength among different cell populations
@@ -79,7 +74,7 @@ gg2 <- netVisual_heatmap(cellchat, measure = "weight")
 gg1 + gg2
 
 weight.max <- getMaxWeight(object.list, attribute = c("idents","count"))
-par(mfrow = c(2,2), xpd=TRUE)
+par(mfrow = c(1,3), xpd=TRUE)
 for (i in 1:length(object.list)) {
   netVisual_circle(object.list[[i]]@net$count, weight.scale = T, label.edge= F, edge.weight.max = weight.max[2], edge.width.max = 12, title.name = paste0("Number of interactions - ", names(object.list)[i]))
 }
@@ -96,37 +91,27 @@ patchwork::wrap_plots(plots = gg)
 
 ### Visualise which pathways are active in the most significant cell states
 
-####SERPINE+Basal
-gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "SERPINE1+Basal", signaling.exclude = "MIF", comparison = c(1,2))
-gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "SERPINE1+Basal", signaling.exclude = "MIF", comparison = c(3,4))
-gg3 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "SERPINE2+Basal", signaling.exclude = "MIF", comparison = c(1,2))
-gg4 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "SERPINE2+Basal", signaling.exclude = "MIF", comparison = c(3,4))
-patchwork::wrap_plots(plots = list(gg1,gg2,gg3,gg4))
 
-gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "iavAPC_Epi", signaling.exclude = "MIF", comparison = c(1,2))
-gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "iavAPC_Epi", signaling.exclude = "MIF", comparison = c(3,4))
-gg3 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "MHCII+Club", signaling.exclude = "MIF", comparison = c(1,2))
-gg4 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "MHCII+Club", signaling.exclude = "MIF", comparison = c(3,4))
-patchwork::wrap_plots(plots = list(gg1,gg2,gg3,gg4))
-
-gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "TNC+Basal", signaling.exclude = "MIF", comparison = c(1,2))
-gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "TNC+Basal", signaling.exclude = "MIF", comparison = c(3,4))
-gg3 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "FB-like_Basal", signaling.exclude = "MIF", comparison = c(1,2))
-gg4 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "FB-like_Basal", signaling.exclude = "MIF", comparison = c(3,4))
-patchwork::wrap_plots(plots = list(gg1,gg2,gg3,gg4))
+gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "vCM3", signaling.exclude = "MIF", comparison = c(1,2))
+gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "vCM3", signaling.exclude = "MIF", comparison = c(1,3))
+gg3 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "Ly6ChiMo", signaling.exclude = "MIF", comparison = c(1,2))
+gg4 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "Ly6ChiMo", signaling.exclude = "MIF", comparison = c(1,3))
+gg5 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "CD8Temra", signaling.exclude = "MIF", comparison = c(1,2))
+gg6 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "CD8Temra", signaling.exclude = "MIF", comparison = c(1,3))
+patchwork::wrap_plots(plots = list(gg1, gg3, gg5, gg2, gg4, gg6))
 
 ###Identify signaling groups based on their functional similarity
 
-cellchat <- computeNetSimilarityPairwise(cellchat, type = "functional", comparison = c(1,2,3,4))
-cellchat <- netEmbedding(cellchat, type = "functional", comparison = c(1,2,3,4))
-cellchat <- netClustering(cellchat, type = "functional", comparison = c(1,2,3,4))
+cellchat <- computeNetSimilarityPairwise(cellchat, type = "functional", comparison = c(1,2,3))
+cellchat <- netEmbedding(cellchat, type = "functional", comparison = c(1,2,3))
+cellchat <- netClustering(cellchat, type = "functional", comparison = c(1,2,3))
 netVisual_embeddingPairwise(cellchat, type = "functional", label.size = 3.5)
 
 ###Identify signaling groups based on their structural similarity
 
-cellchat <- computeNetSimilarityPairwise(cellchat, type = "structural", comparison = c(1,2,3,4))
-cellchat <- netEmbedding(cellchat, type = "structural", comparison = c(1,2,3,4))
-cellchat <- netClustering(cellchat, type = "structural", comparison = c(1,2,3,4))
+cellchat <- computeNetSimilarityPairwise(cellchat, type = "structural", comparison = c(1,2,3))
+cellchat <- netEmbedding(cellchat, type = "structural", comparison = c(1,2,3))
+cellchat <- netClustering(cellchat, type = "structural", comparison = c(1,2,3))
 netVisual_embeddingPairwise(cellchat, type = "structural", label.size = 3.5)
 
 netVisual_embeddingPairwiseZoomIn(cellchat, type = "structural", nCol = 2)
@@ -136,8 +121,8 @@ rankSimilarity(cellchat, type = "structural")
 
 ### Compare the overall information flow of each signaling pathway
 
-gg1 <- rankNet(cellchat, mode = "comparison", stacked = T, do.stat = TRUE, comparison = c(1,2,3,4))
-gg2 <- rankNet(cellchat, mode = "comparison", stacked = F, do.stat = TRUE, comparison = c(1,2,3,4))
+gg1 <- rankNet(cellchat, mode = "comparison", stacked = T, do.stat = TRUE, comparison = c(1,2,3))
+gg2 <- rankNet(cellchat, mode = "comparison", stacked = F, do.stat = TRUE, comparison = c(1,2,3))
 gg1 + gg2
 
 ### Compare outgoing (or incoming) signaling associated with each cell population
@@ -181,23 +166,23 @@ draw(ht3 + ht4, ht_gap = unit(0.5, "cm"))
 ### Identify dysfunctional signaling by comparing the communication probabities
 
 netVisual_bubble(cellchat, 
-                 sources.use = c('SERPINE1+Basal', 'SERPINE2+Basal', 'iavAPC_Epi', 'MHCII+Club', 'TNC+Basal', 'FB-like_Basal'), 
-                 targets.use = c('SERPINE1+Basal', 'SERPINE2+Basal', 'iavAPC_Epi', 'MHCII+Club', 'TNC+Basal', 'FB-like_Basal'), 
-                 comparison = c(1,2,3,4), 
+                 sources.use = c('vCM3'), 
+                 targets.use = c('Ly6ChiMo', 'CD8Temra'), 
+                 comparison = c(1,2,3), 
                  angle.x = 45)
 
 
 ### Identify dysfunctional signaling by using differential expression analysis
 
 # define a positive dataset, i.e., the dataset with positive fold change against the other dataset
-pos.dataset = "healthy_ctrl"
+pos.dataset = "WT"
 
 # define a char name used for storing the results of differential expression analysis
 features.name = pos.dataset
 
 # perform differential expression analysis 
 cellchat <- identifyOverExpressedGenes(cellchat, 
-                                       group.dataset = "group", 
+                                       group.dataset = "genotype", 
                                        pos.dataset = pos.dataset, 
                                        features.name = features.name, 
                                        only.pos = FALSE, 
